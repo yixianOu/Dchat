02dff504877a24eebdcb5eb669906caa6c132166
orician <orician@nexsea.net>
2025-07-15 23:17:19 +0800

    feat: 添加主程序脚本，配置 NATS 服务器和叶节点

---
+++ cmd/leafNode/main.sh
@@ -0,0 +1,54 @@
+set -euo pipefail
+
+NATS_MAIN_URL="nats://0.0.0.0:4222"
+NATS_LEAF_URL="nats://0.0.0.0:4223"
+
+nats context save main \
+  --server "$NATS_MAIN_URL" \
+nats context save leaf \
+  --server "$NATS_LEAF_URL"
+
+echo 'Creating the main server conf...'
+cat <<- EOF > main.conf
+port: 4222
+leafnodes: {
+  port: 7422
+}
+EOF
+echo 'Creating the leaf node conf...'
+cat <<- EOF > leaf.conf
+port: 4223
+leafnodes: {
+  remotes: [
+    {url: "nats-leaf://0.0.0.0:7422"}
+  ]
+}
+EOF
+
+nats-server -c main.conf 2> /dev/null &
+MAIN_PID=$!
+sleep 1
+
+nats-server -c leaf.conf 2> /dev/null &
+LEAF_PID=$!
+sleep 1
+
+nats --context main reply 'greet' 'hello from main' &
+SERVICE_PID=$!
+sleep 1
+
+nats --context leaf request 'greet' ''
+nats --context leaf reply 'greet' 'hello from leaf' &
+SERVICE2_PID=$!
+
+nats --context leaf request 'greet' ''
+nats --context leaf request 'greet' ''
+nats --context leaf request 'greet' ''
+
+kill $SERVICE2_PID
+nats --context leaf request 'greet' ''
+
+kill $SERVICE_PID
+kill $LEAF_PID
+kill $MAIN_PID
+
