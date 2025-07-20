a489943bd3c8723297ec91d27945749d88dd5c22
orician <orician@nexsea.net>
2025-07-20 13:43:42 +0800

    feat: 初始化主函数并设置 NATS 连接参数

--- cmd/LeafJWT/main.go
+++ cmd/LeafJWT/main.go
@@ -1,1 +1,16 @@
 package leafjwt
+
+import "github.com/nats-io/nats.go"
+
+func main() {
+	const (
+		address     = "localhost"
+		mainPort    = 4222
+		clusterPort = 7422
+		leafPort    = 4223
+		subjectName = "foo"
+	)
+
+	mainClientOpt := nats.Options{}
+
+}
