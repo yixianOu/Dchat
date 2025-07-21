f8de50c1aff49bdb87fdfa92bd25d74fbec81357
orician <orician@nexsea.net>
2025-07-21 18:35:35 +0800

    feat: 添加主服务器和叶服务器的创建与启动功能，并执行nsc命令以推送应用

--- cmd/LeafJWT/main.go
+++ cmd/LeafJWT/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/url"
+	"os/exec"
 	"time"
 
 	"github.com/nats-io/nats-server/v2/server"
@@ -22,6 +24,7 @@ func main() {
 		subjectName      = "foo"
 		credsFileAppUser = "~/.local/share/nats/nsc/keys/creds/local/APP/user.creds"
 		credsFileSysUser = "/home/orician/.local/share/nats/nsc/keys/creds/local/SYS/sys.creds"
+		resolverFile     = "/home/orician/workspace/learn/nats/Dchat/cmd/leafNode/resolver.conf"
 	)
 
 	mainClientOpt := NATSClient{
@@ -40,6 +43,73 @@ func main() {
 			nats.MaxReconnects(-1),
 		},
 	}
-	mainServerOpt := server.Options{}
+	mainServerOpt := server.Options{
+		ConfigFile: resolverFile,
+		Host:       address,
+		Port:       mainPort,
+		LeafNode: server.LeafNodeOpts{
+			Port:      clusterPort,
+			Advertise: fmt.Sprintf("%s:%d", address, clusterPort),
+		},
+	}
+	leafServerOpt := server.Options{
+		Host: address,
+		Port: leafPort,
+		LeafNode: server.LeafNodeOpts{
+			Remotes: []*server.RemoteLeafOpts{
+				{
+					URLs:        []*url.URL{{Scheme: "nats", Host: fmt.Sprintf("nats://%s:%d", address, clusterPort)}},
+					Credentials: credsFileAppUser,
+				},
+			},
+		},
+	}
 
+	// 创建并启动main服务器
+	mainServer, err := server.NewServer(&mainServerOpt)
+	if err != nil {
+		panic(fmt.Sprintf("创建主服务器失败: %v", err))
+	}
+	go mainServer.Start()
+	defer mainServer.Shutdown()
+	// 等待服务器完全启动
+	time.Sleep(5 * time.Second)
+	// 执行nsc cli命令: nsc push -a APP
+	execCmd := exec.Command("nsc", "push", "-a", "APP")
+	output, err := execCmd.CombinedOutput()
+	if err != nil {
+		panic(fmt.Sprintf("执行nsc命令失败: %v\n输出: %s", err, string(output)))
+	}
+	fmt.Printf("nsc命令输出:\n%s\n", string(output))
+	// 创建并启动leaf服务器
+	leafServer, err := server.NewServer(&leafServerOpt)
+	if err != nil {
+		panic(fmt.Sprintf("创建叶服务器失败: %v", err))
+	}
+	go leafServer.Start()
+	defer leafServer.Shutdown()
+	// 创建NATS连接
+	mainConn, err := nats.Connect(mainClientOpt.url, mainClientOpt.opts...)
+	if err != nil {
+		panic(fmt.Sprintf("连接到主服务器失败: %v", err))
+	}
+	defer mainConn.Drain()
+	mainSub, err := mainConn.Subscribe(subjectName, func(msg *nats.Msg) {
+		fmt.Printf("主客户端接收到消息: %s\n", string(msg.Data))
+		msg.Respond([]byte("Hello from main client!"))
+	})
+	if err != nil {
+		panic(fmt.Sprintf("在主客户端创建订阅失败: %v", err))
+	}
+	defer mainSub.Drain()
+	leafConn, err := nats.Connect(leafClientOpt.url, leafClientOpt.opts...)
+	if err != nil {
+		panic(fmt.Sprintf("连接到叶服务器失败: %v", err))
+	}
+	defer leafConn.Drain()
+	respond, err := leafConn.Request(subjectName, []byte("Hello from leaf client!"), time.Second)
+	if err != nil {
+		panic(fmt.Sprintf("在叶客户端创建请求失败: %v", err))
+	}
+	fmt.Printf("叶客户端接收到响应: %s\n", string(respond.Data))
 }
